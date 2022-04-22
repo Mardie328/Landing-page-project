d94035c0c41cb221a1cded3da8f3579b1b275bcc
Taofeek Mardiyyah <92341910+Mardie328@users.noreply.github.com>
2022-04-22 14:48:14 +0100

    Add files via upload

---
+++ loadingbutton.js
@@ -0,0 +1,24 @@
+﻿$('#contactme').submit(function (e) {
+    e.preventDefault();
+    var data = {
+        name: $('#Enter_fullname').val(),
+        email: $('#Email_email').val(),
+    }
+    if (data.name != "" && data.email) {
+      send();
+    }    
+    else {
+        swal("Not yet", "All fields are required", "warning");
+    }
+});
+
+function send() {
+   $("#submit").html("<i class = 'fa fa-spinner fa-spin'></i> Please Wait").css('margin', '10px');
+   $("#submit").attr("disabled", "disabled");
+   window.setTimeout(function () {
+    swal("Your message has been sent", "", "success");
+      $("#submit").removeAttr("disabled");
+       $("#submit").html("sent");
+   }, 3000);
+};
+
